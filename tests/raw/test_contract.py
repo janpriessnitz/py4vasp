991f8379564f947d0fce6aab48eb5e1b7fa8543c
Martin Schlipf <martin.schlipf@gmail.com>
2022-06-27 09:26:07 +0200

    Check if there is an example for all parts of py4vasp
    NOTE: this test is currently broken, but we will add more examples until 
    this passes

--- tests/raw/test_contract.py
+++ tests/raw/test_contract.py
@@ -1,13 +1,19 @@
-from py4vasp.raw import _contract
+from py4vasp.raw import _contract, _definition
 from unittest.mock import patch
+import pytest
 
 
 def return_generated_sources(example):
     return example.sources
 
 
+@pytest.mark.xfail(reason="not all examples are generated yet")
 @patch("py4vasp.raw._contract.run_example", side_effect=return_generated_sources)
 def test_examples_cover_all_sources(mock_run):
+    expected_sources = set()
+    for quantity, sources in _definition.schema.sources.items():
+        expected_sources |= {(quantity, source) for source in sources}
     covered_sources = set()
     for example in _contract.EXAMPLES.values():
         covered_sources |= _contract.run_example(example)
+    assert expected_sources == covered_sources
