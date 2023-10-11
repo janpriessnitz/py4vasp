cb5894da8cf416865a0b0508bf3320f0db87e9b5
Martin Schlipf <martin.schlipf@gmail.com>
2023-10-11 14:41:40 +0200

    Implement basic parsing of single line tags

--- src/py4vasp/_control/incar.py
+++ src/py4vasp/_control/incar.py
@@ -12,3 +12,15 @@ class INCAR(base.InputFile):
         Defines where the INCAR file is stored. If set to None, the file will be kept
         in memory.
     """
+
+
+def parse_incar_to_dict(text):
+    return dict(_generate_tags(text))
+
+
+def _generate_tags(text):
+    for line in text.splitlines():
+        line_without_comments, *_ = line.split("#")
+        tag, separator, value = line_without_comments.partition("=")
+        if separator:
+            yield tag.strip().upper(), value.strip()
--- tests/control/test_incar.py
+++ tests/control/test_incar.py
@@ -1,9 +1,23 @@
 # Copyright © VASP Software GmbH,
 # Licensed under the Apache License 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+import pytest
+
 from py4vasp.control import INCAR
+from py4vasp._control.incar import parse_incar_to_dict
 
 from .test_base import AbstractTest
 
 
 class TestIncar(AbstractTest):
     tested_class = INCAR
+
+
+@pytest.mark.parametrize(
+    "input, output",
+    [
+        ("# empty file", {}),
+        ("tag1 = value1 \n tag2 = value2", {"TAG1": "value1", "TAG2": "value2"}),
+    ],
+)
+def test_from_string(input, output):
+    assert parse_incar_to_dict(input) == output
